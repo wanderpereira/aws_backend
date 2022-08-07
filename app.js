43afa24fcc4a136de04bc91ff136d88a08693e7d
Wander Pereira da Silva <wanderpereira@gmx.us>
2022-08-07 00:24:54 -0300

    alterando o projeto

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.port
+import express from "express";
+import cors from "cors"
+import bodyParser from "body-parser"
+import {estados, ranking} from "./database/Model.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+const port = process.env.port;
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(bodyParser.json())
+
+app.get('/estados', (req, res) => {
+    estados().then((results) => {
+        res.json(results)
+    });
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/estados', (req, res) => {
+
+    const {sigla, geo } = req.body;
+    
+    ranking(sigla, geo).then((results) => {
+        res.json(`${results[0].estado} é o ${results[0].ranking}º estado do ranking da região ${results[0].regiao} contendo uma população igual à ${results[0].populacao} de habitantes.`)
+    });
 })
+
+
+app.listen(port);
+console.log('Servidor rodando em http://localhost:' + port);
